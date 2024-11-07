cc27dd7bed905eebdb5186772c012ff91c4ecac5
Hugo S <20hugosousa16@gmail.com>
2024-11-07 11:13:29 -0300

    renomeação de classe

--- src/br/com/alura/conversosdemoedas/classes/BuscaJson.java
+++ src/br/com/alura/conversosdemoedas/classes/BuscaJson.java
@@ -6,13 +6,12 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-public class BuscaTaxas {
-    public String getApi (String taxa) throws IOException, InterruptedException {
+public class BuscaJson {
+    public String getJson(String currencyCode) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://v6.exchangerate-api.com/v6/602657242789e434d3e9dac4/latest/" + taxa)).build();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://v6.exchangerate-api.com/v6/602657242789e434d3e9dac4/latest/" + currencyCode)).build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        String json = response.body();
-        return json;
+        return response.body();
     }
 }
 
