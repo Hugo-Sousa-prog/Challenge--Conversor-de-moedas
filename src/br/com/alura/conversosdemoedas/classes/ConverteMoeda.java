20914d74535e10148bc81f9fdbb6419dee9f1779
Hugo S <20hugosousa16@gmail.com>
2024-11-08 13:06:01 -0300

    classe para realizar calculo de conversao

---
+++ src/br/com/alura/conversosdemoedas/classes/ConverteMoeda.java
@@ -0,0 +1,7 @@
+package br.com.alura.conversosdemoedas.classes;
+
+public class ConverteMoeda {
+    public double conversorMoeda (double moeda, double taxa){
+        return moeda * taxa;
+    }
+}
