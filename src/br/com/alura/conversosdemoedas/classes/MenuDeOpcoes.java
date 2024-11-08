40d4ee2f1cc72a3591a1b8d31381e57725435501
Hugo S <20hugosousa16@gmail.com>
2024-11-08 13:06:59 -0300

    classe para realizar o menu

---
+++ src/br/com/alura/conversosdemoedas/classes/MenuDeOpcoes.java
@@ -0,0 +1,16 @@
+package br.com.alura.conversosdemoedas.classes;
+
+public class MenuDeOpcoes {
+    public void exibeMenu(){
+        System.out.println("╔═══════════      Menu de Opções       ═══════════╗");
+        System.out.println("║  0. Sair                                        ║");
+        System.out.println("║  1. Real brasileiro (BRL) == Iene (JPY)         ║");
+        System.out.println("║  2. Real brasileiro (BRL) == Euro (EUR)         ║");
+        System.out.println("║  3. Real brasileiro (BRL) == Dólar (USD)        ║");
+        System.out.println("║  4. Iene (JPY) == Real brasileiro (BRL)         ║");
+        System.out.println("║  5. Euro (EUR) == Real brasileiro (BRL)         ║");
+        System.out.println("║  6. Dólar (USD) == Real brasileiro (BRL)        ║");
+        System.out.println("╚═════════════════════════════════════════════════╝");
+        System.out.println("════ Escolha uma das opções acima de conversão ════");
+    }
+}
