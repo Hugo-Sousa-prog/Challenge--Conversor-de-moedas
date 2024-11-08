616dd4f3e312c399464da9d618b42c8b998e4f2d
Hugo S <20hugosousa16@gmail.com>
2024-11-08 13:07:38 -0300

    instansciacao de classe nova

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,23 @@
 import br.com.alura.conversosdemoedas.classes.*;
 
+import java.awt.*;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        BuscaJson buscaJson = new BuscaJson();
-        SelecionaTaxa selecionaTaxa = new SelecionaTaxa();
-
+//        int opcao;
+//        String json;
+//        double valorTaxa;
+//        double valorMoeda;
+//        double moedaConvertida;
+//
+//        BuscaJson buscaJson = new BuscaJson();
+//        SelecionaTaxa selecionaTaxa = new SelecionaTaxa();
+//        ConverteMoeda converteMoeda = new ConverteMoeda();
+//        MenuDeOpcoes menuDeOpcoes = new MenuDeOpcoes();
+//        Scanner scanner = new Scanner(System.in);
+        ExecutaConversao executaConversao = new ExecutaConversao();
+        executaConversao.executa();
     }
 }
