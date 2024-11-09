cb26eb66aff6b649ea1687c20c1c737203d8d606
Hugo S2 <20hugosousa16@gmail.com>
2024-11-09 17:03:21 -0300

    otimizando o código

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,10 @@
 import br.com.alura.conversosdemoedas.classes.*;
 
-import java.awt.*;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-//        int opcao;
-//        String json;
-//        double valorTaxa;
-//        double valorMoeda;
-//        double moedaConvertida;
-//
-//        BuscaJson buscaJson = new BuscaJson();
-//        SelecionaTaxa selecionaTaxa = new SelecionaTaxa();
-//        ConverteMoeda converteMoeda = new ConverteMoeda();
-//        MenuDeOpcoes menuDeOpcoes = new MenuDeOpcoes();
-//        Scanner scanner = new Scanner(System.in);
         ExecutaConversao executaConversao = new ExecutaConversao();
-        executaConversao.executa();
+            executaConversao.executaConversao();
     }
 }
--- src/br/com/alura/conversosdemoedas/classes/ExecutaConversao.java
+++ src/br/com/alura/conversosdemoedas/classes/ExecutaConversao.java
@@ -4,11 +4,13 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class ExecutaConversao {
-    int opcao;
-    String json;
-    double valorTaxa;
-    double valorMoeda;
-    double moedaConvertida;
+    private int opcao;
+    private String json;
+    private String taxa;
+    private String moeda;
+    private double valorTaxa;
+    private double valorMoeda;
+    private double moedaConvertida;
 
     BuscaJson buscaJson = new BuscaJson();
     SelecionaTaxa selecionaTaxa = new SelecionaTaxa();
@@ -16,7 +18,31 @@ public class ExecutaConversao {
     MenuDeOpcoes menuDeOpcoes = new MenuDeOpcoes();
     Scanner scanner = new Scanner(System.in);
 
-    public void executa() throws IOException, InterruptedException {
+    public void escolhendoTaxaValor() throws IOException, InterruptedException {
+        System.out.println("\nDigite o valor que deseja converter: ");
+        this.valorMoeda = scanner.nextDouble();
+        this.json = buscaJson.getJson(this.moeda);
+        this.valorTaxa = selecionaTaxa.getRate(this.json, this.taxa);
+        this.moedaConvertida = converteMoeda.conversorMoeda(valorMoeda, valorTaxa);
+    }
+
+    public void retornandoMoedaConvertida(){
+        if (this.moeda.equals("BRL") && this.taxa.equals("JPY")){
+            System.out.println("\nR$" + this.valorMoeda + " convertido para iene é igual a: " + "¥" + this.moedaConvertida + "\n");
+        } else if (this.moeda.equals("BRL") && this.taxa.equals("EUR")) {
+            System.out.println("\nR$" + this.valorMoeda + " convertido para euro é igual a: " + "€" + this.moedaConvertida + "\n");
+        } else if (this.moeda.equals("BRL") && this.taxa.equals("USD")) {
+            System.out.println("\nR$" + this.valorMoeda + " convertido para dólar é igual a: " + "$" + this.moedaConvertida + "\n");
+        } else if (this.moeda.equals("JPY") && this.taxa.equals("BRL")) {
+            System.out.println("\n¥" + this.valorMoeda + " convertido para real é igual a: " + "R$" + this.moedaConvertida + "\n");
+        } else if (this.moeda.equals("EUR") && this.taxa.equals("BRL")) {
+            System.out.println("\n€" + this.valorMoeda + " convertido para euro é igual a: " + "R$" + this.moedaConvertida + "\n");
+        } else if (this.moeda.equals("USD") && this.taxa.equals("BRL")) {
+            System.out.println("\n$" + this.valorMoeda + " convertido para dólar é igual a: " + "R$" + this.moedaConvertida + "\n");
+        }
+    }
+
+    public void executaConversao() throws IOException, InterruptedException {
         try {
             do {
                 menuDeOpcoes.exibeMenu();
@@ -28,65 +54,53 @@ public class ExecutaConversao {
                         break;
 
                     case 1:
-                        System.out.println("\nDigite o valor que deseja converter: ");
-                        valorMoeda = scanner.nextDouble();
-                        json = buscaJson.getJson("BRL");
-                        valorTaxa = selecionaTaxa.getRate(json, "JPY");
-                        moedaConvertida = converteMoeda.conversorMoeda(valorMoeda, valorTaxa);
-                        System.out.println("\nR$" + valorMoeda + " convertido para iene é igual a: " + "¥" + moedaConvertida + "\n");
+                        this.moeda = "BRL";
+                        this.taxa = "JPY";
+                        escolhendoTaxaValor();
+                        retornandoMoedaConvertida();
                         break;
 
                     case 2:
-                        System.out.println("\nDigite o valor que deseja converter: ");
-                        valorMoeda = scanner.nextDouble();
-                        json = buscaJson.getJson("BRL");
-                        valorTaxa = selecionaTaxa.getRate(json, "EUR");
-                        moedaConvertida = converteMoeda.conversorMoeda(valorMoeda, valorTaxa);
-                        System.out.println("\nR$" + valorMoeda + " convertido para euro é igual a: " + "€" + moedaConvertida + "\n");
+                        this.moeda = "BRL";
+                        this.taxa = "EUR";
+                        escolhendoTaxaValor();
+                        retornandoMoedaConvertida();
                         break;
 
                     case 3:
-                        System.out.println("\nDigite o valor que deseja converter: ");
-                        valorMoeda = scanner.nextDouble();
-                        json = buscaJson.getJson("BRL");
-                        valorTaxa = selecionaTaxa.getRate(json, "USD");
-                        moedaConvertida = converteMoeda.conversorMoeda(valorMoeda, valorTaxa);
-                        System.out.println("\nR$" + valorMoeda + " convertido para dólar é igual a: " + "$" + moedaConvertida + "\n");
+                        this.moeda = "BRL";
+                        this.taxa = "USD";
+                        escolhendoTaxaValor();
+                        retornandoMoedaConvertida();
                         break;
 
                     case 4:
-                        System.out.println("\nDigite o valor que deseja converter: ");
-                        valorMoeda = scanner.nextDouble();
-                        json = buscaJson.getJson("JPY");
-                        valorTaxa = selecionaTaxa.getRate(json, "BRL");
-                        moedaConvertida = converteMoeda.conversorMoeda(valorMoeda, valorTaxa);
-                        System.out.println("\n¥" + valorMoeda + " convertido para real é igual a: " + "R$" + moedaConvertida + "\n");
+                        this.moeda = "JPY";
+                        this.taxa = "BRL";
+                        escolhendoTaxaValor();
+                        retornandoMoedaConvertida();
                         break;
 
                     case 5:
-                        System.out.println("\nDigite o valor que deseja converter: ");
-                        valorMoeda = scanner.nextDouble();
-                        json = buscaJson.getJson("EUR");
-                        valorTaxa = selecionaTaxa.getRate(json, "BRL");
-                        moedaConvertida = converteMoeda.conversorMoeda(valorMoeda, valorTaxa);
-                        System.out.println("\n€" + valorMoeda + " convertido para euro é igual a: " + "R$" + moedaConvertida + "\n");
+                        this.moeda = "EUR";
+                        this.taxa = "BRL";
+                        escolhendoTaxaValor();
+                        retornandoMoedaConvertida();
                         break;
 
                     case 6:
-                        System.out.println("\nDigite o valor que deseja converter: ");
-                        valorMoeda = scanner.nextDouble();
-                        json = buscaJson.getJson("USD");
-                        valorTaxa = selecionaTaxa.getRate(json, "BRL");
-                        moedaConvertida = converteMoeda.conversorMoeda(valorMoeda, valorTaxa);
-                        System.out.println("\n$" + valorMoeda + " convertido para dólar é igual a: " + "R$" + moedaConvertida + "\n");
+                        this.moeda = "USD";
+                        this.taxa = "BRL";
+                        escolhendoTaxaValor();
+                        retornandoMoedaConvertida();
                         break;
                     default:
                         System.out.println("\nOpção inválida, digite outra opção");
                 }
             } while (opcao != 0);
-            
+
         } catch (RuntimeException e) {
-            System.out.println("Erro de conversão: " + e.getMessage());
+            System.out.println("Um erro foi encontrado: " + e.getMessage());
         }
     }
 }
